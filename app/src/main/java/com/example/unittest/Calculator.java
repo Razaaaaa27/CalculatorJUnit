5d3ef09d64a17d2579ca8957ac60d472b55b1edc
Muhammad Raza Adzani <163233947+Razaaaaa27@users.noreply.github.com>
2025-03-09 12:32:23 +0700

    Update Calculator.java

--- app/src/main/java/com/example/unittest/Calculator.java
+++ app/src/main/java/com/example/unittest/Calculator.java
@@ -2,18 +2,23 @@ package com.example.unittest;
 
 public class Calculator {
 
+    // Metode untuk melakukan penjumlahan dua bilangan
     public int add(int num1, int num2) {
         return num1 + num2;
     }
 
+    // Metode untuk melakukan pengurangan dua bilangan
     public int subtract(int num1, int num2) {
         return num1 - num2;
     }
 
+    // Metode untuk melakukan perkalian dua bilangan
     public int multiply(int num1, int num2) {
         return num1 * num2;
     }
 
+    // Metode untuk melakukan pembagian dua bilangan
+    // Melemparkan ArithmeticException jika pembagi adalah nol
     public double divide(int num1, int num2) {
         if (num2 == 0) {
             throw new ArithmeticException("Cannot divide by zero");
